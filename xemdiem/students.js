429a77b6cce7fdd547afaed0259f548ddace350c
Anhtx1u11 <ffnat151515@gmail.com>
2025-05-02 23:53:16 -0700

    Add files via upload

--- xemdiem/students.js
+++ xemdiem/students.js
@@ -35,8 +35,50 @@ const students = {
   "K16THO0069": "Trần Hữu Trung",
   "K16THO0070": "Nguyễn Anh Tú",
   "K16THO0071": "Lê Vũ",
-  "K16THO0072": "Đoàn Quang Vương"
+  "K16THO0072": "Đoàn Quang Vương",
+
+  // Dưới đây là 39 sinh viên mới
+  "K16THO0003": "Nguyễn Bùi Đức",
+  "K16THO0004": "Nguyễn Hoàng Anh",
+  "K16THO0005": "Huỳnh Gia Bảo",
+  "K16THO0006": "Nguyễn Thanh Bình",
+  "K16THO0010": "Huỳnh Hoàng Đức",
+  "K16THO0013": "Nguyễn Quốc Hải",
+  "K16THO0014": "Võ Thanh Hậu",
+  "K16THO0015": "Nguyễn Thành Hiếu",
+  "K16THO0016": "Phạm Xuân Hiếu",
+  "K16THO0025": "Phạm Văn Huy",
+  "K16THO0026": "Phan Gia Huy",
+  "K16THO0027": "Phùng Nhất Huy",
+  "K16THO0028": "Trương Minh Khải",
+  "K16THO0029": "Nguyễn Tuấn Khanh",
+  "K16THO0023": "Ngô Lê Huy",
+  "K16THO0031": "Phan Vũ Anh Khôi",
+  "K16THO0035": "Phạm Ngọc Minh",
+  "K16THO0036": "Nguyễn Nhật Nam",
+  "K16THO0033": "Nguyễn Lưu Thành Lộc",
+  "K16THO0037": "Lê Nhân",
+  "K16THO0034": "Nguyễn Trần Đại Long",
+  "K16THO0045": "Nguyễn Anh Quân",
+  "K16THO0046": "Nguyễn Nhất Sinh",
+  "K16THO0047": "Dương Tấn Tài",
+  "K16THO0048": "Nguyễn Hữu Tài",
+  "K16THO0051": "Trần Thanh Tấn",
+  "K16THO0052": "Nguyễn Xuân Ngọc Thắm",
+  "K16THO0074": "Đỗ Ngọc Thạch",
+  "K16THO0054": "Trịnh Tấn Thành",
+  "K16THO0055": "Nguyễn Quốc Thể",
+  "K16THO0056": "Ngô Văn Thịnh",
+  "K16THO0057": "Lê Văn Thống",
+  "K16THO0062": "Phạm Trọng Tín",
+  "K16THO0063": "Ngô Ngọc Bảo Trân",
+  "K16THO0064": "Lê Phạm Hoàng Triều",
+  "K16THO0065": "Lê Thị Mỹ Trinh",
+  "K16THO0066": "Nguyễn Khúc Thanh Trọng",
+  "K16THO0067": "Trần Hữu Trọng",
+  "K16THO0075": "Nguyễn Hoàng Trung Hiên"
 };
+
 
 // Lấy MSSV từ query string, ví dụ: index.html?mssv=K16THO0042
 function getMSSVFromURL() {
